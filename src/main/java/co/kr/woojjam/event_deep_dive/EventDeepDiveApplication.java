90d992f94743c82c85c77b3483f9ee90748067e9
WooJJam <111514410+WooJJam@users.noreply.github.com>
2026-04-13 15:22:19 +0900

    init: 프로젝트 생성

---
+++ src/main/java/co/kr/woojjam/event_deep_dive/EventDeepDiveApplication.java
@@ -0,0 +1,13 @@
+package co.kr.woojjam.event_deep_dive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDeepDiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventDeepDiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/kr/woojjam/event_deep_dive/EventDeepDiveApplicationTests.java
@@ -0,0 +1,13 @@
+package co.kr.woojjam.event_deep_dive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDeepDiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
