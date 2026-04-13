be545f4d79f3689546095c58f9b345816b3273d6
WooJJam <111514410+WooJJam@users.noreply.github.com>
2026-04-13 23:16:18 +0900

    feat: 프로젝트 기본 설정 (MySQL, 스케줄러 활성화)

--- src/main/java/co/kr/woojjam/event_deep_dive/EventDeepDiveApplication.java
+++ src/main/java/co/kr/woojjam/event_deep_dive/EventDeepDiveApplication.java
@@ -2,8 +2,10 @@ package co.kr.woojjam.event_deep_dive;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class EventDeepDiveApplication {
 
 	public static void main(String[] args) {
